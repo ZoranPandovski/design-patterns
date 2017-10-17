33ed06c84a1b09a8c0d90cfb186d07ca60cd0ce7
Trustgeek <sagarduwal0102@hotmail.com>
2017-10-17 23:29:24 +0545

    Adapter Design pattern with AnimalAdapter and ShapeAdapter

---
+++ Structural/Adapter/java/AnimalAdapterDemo.java
@@ -0,0 +1,60 @@
+package com.trustgeek.design_patterns.Structural.Adapter;
+
+/**
+ * Created by trustgeek on 10/17/2017 {11:08 PM}.
+ */
+interface Animal {
+    void action(int forelimbs, int hindlimbs, String act);
+}
+
+class Bird {
+    void action(int forelimbs, int hindlimbs, String act) {
+        System.out.println("I have " + forelimbs + " forelimbs and " + hindlimbs + " hindlimbs. I can "+ act);
+    }
+}
+
+class Human {
+    void action(int forelimbs, int hindlimbs, String act) {
+        System.out.println("I have " + forelimbs + " forelimbs and " + hindlimbs + " hindlimbs. I can "+ act);
+    }
+}
+
+class BirdAdapter implements Animal {
+    private Bird adaptee;
+
+    BirdAdapter(Bird dog) {
+        this.adaptee = dog;
+    }
+
+    @Override
+    public void action(int forelimbs, int hindlimbs, String act) {
+        String actionAct = act + " to fly" ;
+        adaptee.action(forelimbs, hindlimbs, actionAct);
+    }
+}
+
+class HumanAdapter implements Animal {
+    private Human adaptee;
+
+    HumanAdapter(Human human) {
+        this.adaptee = human;
+    }
+
+    @Override
+    public void action(int forelimbs, int hindlimbs, String act) {
+        String actionAct = act + " to run";
+        adaptee.action(forelimbs, hindlimbs, actionAct);
+    }
+}
+
+public class AdapterDemo2 {
+    public static void main(String[] args) {
+        Animal[] animals = {new HumanAdapter(new Human()),
+                new BirdAdapter(new Bird())};
+        int forelimbs = 2, hindlimbs = 2;
+        String act = "move";
+        for (Animal animal: animals) {
+            animal.action(forelimbs, hindlimbs, act);
+        }
+    }
+}
---
+++ Structural/Adapter/java/ShapeAdapterDemo.java
@@ -0,0 +1,63 @@
+package com.trustgeek.design_patterns.Structural.Adapter;
+
+/**
+ * Created by trustgeek on 10/17/2017 {11:02 PM}.
+ */
+interface Shape {
+    void draw(int x, int y, int z, int a);
+}
+
+class Line {
+    public void draw(int x1, int y1, int x2, int y2) {
+        System.out.println("Line from point A(" + x1 + ";" + y1 + "), to point B(" + x2 + ";" + y2 + ")");
+    }
+}
+
+class Rectangle {
+    public void draw(int x, int y, int width, int height) {
+        System.out.println("Rectangle with coordinate left-down point (" + x + ";" + y + "), width: " + width
+                + ", height: " + height);
+    }
+}
+
+class LineAdapter implements Shape {
+    private Line shape;
+
+    public LineAdapter(Line line) {
+        this.shape = line;
+    }
+
+    @Override
+    public void draw(int x1, int y1, int x2, int y2) {
+        shape.draw(x1, y1, x2, y2);
+    }
+}
+
+class RectangleAdapter implements Shape {
+    private Rectangle shape;
+
+    public RectangleAdapter(Rectangle rectangle) {
+        this.shape = rectangle;
+    }
+
+    @Override
+    public void draw(int x1, int y1, int x2, int y2) {
+        int x = Math.min(x1, x2);
+        int y = Math.min(y1, y2);
+        int width = Math.abs(x2 - x1);
+        int height = Math.abs(y2 - y1);
+        shape.draw(x, y, width, height);
+    }
+}
+
+public class AdapterDemo {
+    public static void main(String[] args) {
+        Shape[] shapes = {new RectangleAdapter(new Rectangle()),
+                new LineAdapter(new Line())};
+        int x1 = 10, y1 = 20;
+        int x2 = 30, y2 = 60;
+        for (Shape shape : shapes) {
+            shape.draw(x1, y1, x2, y2);
+        }
+    }
+}
