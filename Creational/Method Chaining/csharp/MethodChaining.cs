c3775f7b36ce6a30e5237c8256fd25e006a7e544
Winston <ruto@protonmail.ch>
2017-10-22 21:12:59 -0400

    Add method chaining implementation in C#

---
+++ Creational/Method Chaining/csharp/MethodChaining.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class MethodChaining
+{
+    public static void Main(string[] args)
+    {
+        var bigCircle = new Circle(123.2).IncrementRadius(21.3);
+        var smallCircle = new Circle(5.5).DecrementRadius(4.1);
+
+        Console.WriteLine($"The big circle (radius: {bigCircle.Radius} has an area of {bigCircle.Area}");
+        Console.WriteLine($"The small circle (radius: {smallCircle.Radius} has a circumference of {smallCircle.Circumference}");
+        Console.ReadLine();
+    }
+
+    class Circle
+    {
+        public double Radius;
+
+        public double Area
+        {
+            get => Math.PI * Radius * Radius;
+        }
+
+        public double Circumference
+        {
+            get => 2 * Math.PI * Radius;
+        }
+
+        public Circle(double radius = 0.0)
+        {
+            Radius = radius;
+        }
+
+        public Circle SetRadius(double radius)
+        {
+            Radius = radius;
+            return this;
+        }
+
+        public Circle IncrementRadius(double value)
+        {
+            Radius += value;
+            return this;
+        }
+
+        public Circle DecrementRadius(double value)
+        {
+            Radius -= value;
+            return this;
+        }
+
+    }
+}
