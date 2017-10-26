20a549f07d424c17fec306abdc135aad2d400cce
Sarel van der Walt <sarel.w@gmail.com>
2017-10-26 20:32:25 +0200

    Structural - Adapter

---
+++ Structural/Adapter/php/adapter.php
@@ -0,0 +1,56 @@
+<?php
+interface Lion
+{
+    public function roar();
+}
+
+class AfricanLion implements Lion
+{
+    public function roar()
+    {
+    }
+}
+
+class AsianLion implements Lion
+{
+    public function roar()
+    {
+    }
+}
+
+class Hunter
+{
+    public function hunt(Lion $lion)
+    {
+    }
+}
+
+// This needs to be added to the game
+class WildDog
+{
+    public function bark()
+    {
+    }
+}
+
+// Adapter around wild dog to make it compatible with our game
+class WildDogAdapter implements Lion
+{
+    protected $dog;
+
+    public function __construct(WildDog $dog)
+    {
+        $this->dog = $dog;
+    }
+
+    public function roar()
+    {
+        $this->dog->bark();
+    }
+}
+
+$wildDog = new WildDog();
+$wildDogAdapter = new WildDogAdapter($wildDog);
+
+$hunter = new Hunter();
+$hunter->hunt($wildDogAdapter);
