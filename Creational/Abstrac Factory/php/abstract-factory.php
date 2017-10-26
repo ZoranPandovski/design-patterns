98e2aa5cdb27f4cf346896065d4f5df44866915c
Sarel van der Walt <sarel.w@gmail.com>
2017-10-26 20:23:31 +0200

    Creational - Abstract Factory

---
+++ Creational/Abstrac Factory/php/abstract-factory.php
@@ -0,0 +1,96 @@
+<?php
+interface Door
+{
+    public function getDescription();
+}
+
+class WoodenDoor implements Door
+{
+    public function getDescription()
+    {
+        echo 'I am a wooden door';
+    }
+}
+
+class IronDoor implements Door
+{
+    public function getDescription()
+    {
+        echo 'I am an iron door';
+    }
+}
+
+
+interface DoorFittingExpert
+{
+    public function getDescription();
+}
+
+class Welder implements DoorFittingExpert
+{
+    public function getDescription()
+    {
+        echo 'I can only fit iron doors';
+    }
+}
+
+class Carpenter implements DoorFittingExpert
+{
+    public function getDescription()
+    {
+        echo 'I can only fit wooden doors';
+    }
+}
+
+
+interface DoorFactory
+{
+    public function makeDoor(): Door;
+    public function makeFittingExpert(): DoorFittingExpert;
+}
+
+// Wooden factory to return carpenter and wooden door
+class WoodenDoorFactory implements DoorFactory
+{
+    public function makeDoor(): Door
+    {
+        return new WoodenDoor();
+    }
+
+    public function makeFittingExpert(): DoorFittingExpert
+    {
+        return new Carpenter();
+    }
+}
+
+// Iron door factory to get iron door and the relevant fitting expert
+class IronDoorFactory implements DoorFactory
+{
+    public function makeDoor(): Door
+    {
+        return new IronDoor();
+    }
+
+    public function makeFittingExpert(): DoorFittingExpert
+    {
+        return new Welder();
+    }
+}
+
+
+$woodenFactory = new WoodenDoorFactory();
+
+$door = $woodenFactory->makeDoor();
+$expert = $woodenFactory->makeFittingExpert();
+
+$door->getDescription();  // Output: I am a wooden door
+$expert->getDescription(); // Output: I can only fit wooden doors
+
+// Same for Iron Factory
+$ironFactory = new IronDoorFactory();
+
+$door = $ironFactory->makeDoor();
+$expert = $ironFactory->makeFittingExpert();
+
+$door->getDescription();  // Output: I am an iron door
+$expert->getDescription(); // Output: I can only fit iron doors
