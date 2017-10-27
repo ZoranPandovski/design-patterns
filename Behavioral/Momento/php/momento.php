4ceb7afad8c988f05c69624768b7c4dc2fde51b2
Sarel van der Walt <sarel.w@gmail.com>
2017-10-27 08:37:26 +0200

    Behavioural - Momento

---
+++ Behavioral/Momento/php/momento.php
@@ -0,0 +1,60 @@
+<?php
+class EditorMemento
+{
+    protected $content;
+
+    public function __construct(string $content)
+    {
+        $this->content = $content;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
+
+class Editor
+{
+    protected $content = '';
+
+    public function type(string $words)
+    {
+        $this->content = $this->content . ' ' . $words;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function save()
+    {
+        return new EditorMemento($this->content);
+    }
+
+    public function restore(EditorMemento $memento)
+    {
+        $this->content = $memento->getContent();
+    }
+}
+
+$editor = new Editor();
+
+// Type some stuff
+$editor->type('This is the first sentence.');
+$editor->type('This is second.');
+
+// Save the state to restore to : This is the first sentence. This is second.
+$saved = $editor->save();
+
+// Type some more
+$editor->type('And this is third.');
+
+// Output: Content before Saving
+echo $editor->getContent(); // This is the first sentence. This is second. And this is third.
+
+// Restoring to last saved state
+$editor->restore($saved);
+
+$editor->getContent(); // This is the first sentence. This is second.
