30540558f5f9a7db0bfd3dbaa403c859f35b2643
Sarel van der Walt <sarel.w@gmail.com>
2017-10-27 08:35:03 +0200

    Behavioural - Iterator

---
+++ Behavioral/Iterator/php/iterator.php
@@ -0,0 +1,83 @@
+<?php
+class RadioStation
+{
+    protected $frequency;
+
+    public function __construct(float $frequency)
+    {
+        $this->frequency = $frequency;
+    }
+
+    public function getFrequency(): float
+    {
+        return $this->frequency;
+    }
+}
+
+use Countable;
+use Iterator;
+
+class StationList implements Countable, Iterator
+{
+    /** @var RadioStation[] $stations */
+    protected $stations = [];
+
+    /** @var int $counter */
+    protected $counter;
+
+    public function addStation(RadioStation $station)
+    {
+        $this->stations[] = $station;
+    }
+
+    public function removeStation(RadioStation $toRemove)
+    {
+        $toRemoveFrequency = $toRemove->getFrequency();
+        $this->stations = array_filter($this->stations, function (RadioStation $station) use ($toRemoveFrequency) {
+            return $station->getFrequency() !== $toRemoveFrequency;
+        });
+    }
+
+    public function count(): int
+    {
+        return count($this->stations);
+    }
+
+    public function current(): RadioStation
+    {
+        return $this->stations[$this->counter];
+    }
+
+    public function key()
+    {
+        return $this->counter;
+    }
+
+    public function next()
+    {
+        $this->counter++;
+    }
+
+    public function rewind()
+    {
+        $this->counter = 0;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->stations[$this->counter]);
+    }
+}
+
+$stationList = new StationList();
+
+$stationList->addStation(new RadioStation(89));
+$stationList->addStation(new RadioStation(101));
+$stationList->addStation(new RadioStation(102));
+$stationList->addStation(new RadioStation(103.2));
+
+foreach($stationList as $station) {
+    echo $station->getFrequency() . PHP_EOL;
+}
+
+$stationList->removeStation(new RadioStation(89)); // Will remove station 89
