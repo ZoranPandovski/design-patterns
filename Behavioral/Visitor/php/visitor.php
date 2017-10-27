e0a39bfc6a6c8d443592b00bf8518ee77629c58b
Sarel van der Walt <sarel.w@gmail.com>
2017-10-27 08:41:07 +0200

    Behavioural - Visitor

---
+++ Behavioral/Visitor/php/visitor.php
@@ -0,0 +1,81 @@
+<?php
+// Visitee
+interface Animal
+{
+    public function accept(AnimalOperation $operation);
+}
+
+// Visitor
+interface AnimalOperation
+{
+    public function visitMonkey(Monkey $monkey);
+    public function visitLion(Lion $lion);
+    public function visitDolphin(Dolphin $dolphin);
+}
+
+class Monkey implements Animal
+{
+    public function shout()
+    {
+        echo 'Ooh oo aa aa!';
+    }
+
+    public function accept(AnimalOperation $operation)
+    {
+        $operation->visitMonkey($this);
+    }
+}
+
+class Lion implements Animal
+{
+    public function roar()
+    {
+        echo 'Roaaar!';
+    }
+
+    public function accept(AnimalOperation $operation)
+    {
+        $operation->visitLion($this);
+    }
+}
+
+class Dolphin implements Animal
+{
+    public function speak()
+    {
+        echo 'Tuut tuttu tuutt!';
+    }
+
+    public function accept(AnimalOperation $operation)
+    {
+        $operation->visitDolphin($this);
+    }
+}
+
+class Speak implements AnimalOperation
+{
+    public function visitMonkey(Monkey $monkey)
+    {
+        $monkey->shout();
+    }
+
+    public function visitLion(Lion $lion)
+    {
+        $lion->roar();
+    }
+
+    public function visitDolphin(Dolphin $dolphin)
+    {
+        $dolphin->speak();
+    }
+}
+
+$monkey = new Monkey();
+$lion = new Lion();
+$dolphin = new Dolphin();
+
+$speak = new Speak();
+
+$monkey->accept($speak);    // Ooh oo aa aa!
+$lion->accept($speak);      // Roaaar!
+$dolphin->accept($speak);   // Tuut tutt tuutt!
