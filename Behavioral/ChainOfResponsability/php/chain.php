27c76bab2ea805949902bb5a6eeeb6dbcd9462c4
Sarel van der Walt <sarel.w@gmail.com>
2017-10-27 08:31:17 +0200

    Behavioural - Chain of Responsibility

---
+++ Behavioral/ChainOfResponsability/php/chain.php
@@ -0,0 +1,81 @@
+<?php
+abstract class Account
+{
+    protected $successor;
+    protected $balance;
+
+    public function setNext(Account $account)
+    {
+        $this->successor = $account;
+    }
+
+    public function pay(float $amountToPay)
+    {
+        if ($this->canPay($amountToPay)) {
+            echo sprintf('Paid %s using %s' . PHP_EOL, $amountToPay, get_called_class());
+        } elseif ($this->successor) {
+            echo sprintf('Cannot pay using %s. Proceeding ..' . PHP_EOL, get_called_class());
+            $this->successor->pay($amountToPay);
+        } else {
+            throw new Exception('None of the accounts have enough balance');
+        }
+    }
+
+    public function canPay($amount): bool
+    {
+        return $this->balance >= $amount;
+    }
+}
+
+class Bank extends Account
+{
+    protected $balance;
+
+    public function __construct(float $balance)
+    {
+        $this->balance = $balance;
+    }
+}
+
+class Paypal extends Account
+{
+    protected $balance;
+
+    public function __construct(float $balance)
+    {
+        $this->balance = $balance;
+    }
+}
+
+class Bitcoin extends Account
+{
+    protected $balance;
+
+    public function __construct(float $balance)
+    {
+        $this->balance = $balance;
+    }
+}
+
+// Let's prepare a chain like below
+//      $bank->$paypal->$bitcoin
+//
+// First priority bank
+//      If bank can't pay then paypal
+//      If paypal can't pay then bit coin
+
+$bank = new Bank(100);          // Bank with balance 100
+$paypal = new Paypal(200);      // Paypal with balance 200
+$bitcoin = new Bitcoin(300);    // Bitcoin with balance 300
+
+$bank->setNext($paypal);
+$paypal->setNext($bitcoin);
+
+// Let's try to pay using the first priority i.e. bank
+$bank->pay(259);
+
+// Output will be
+// ==============
+// Cannot pay using bank. Proceeding ..
+// Cannot pay using paypal. Proceeding ..:
+// Paid 259 using Bitcoin!
