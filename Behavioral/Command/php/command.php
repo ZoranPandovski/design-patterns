2db0981cfbee09213806b9f34035925bcb6e789f
Sarel van der Walt <sarel.w@gmail.com>
2017-10-27 08:33:51 +0200

    Behavioural - Command

---
+++ Behavioral/Command/php/command.php
@@ -0,0 +1,89 @@
+<?php
+// Receiver
+class Bulb
+{
+    public function turnOn()
+    {
+        echo "Bulb has been lit";
+    }
+
+    public function turnOff()
+    {
+        echo "Darkness!";
+    }
+}
+
+interface Command
+{
+    public function execute();
+    public function undo();
+    public function redo();
+}
+
+// Command
+class TurnOn implements Command
+{
+    protected $bulb;
+
+    public function __construct(Bulb $bulb)
+    {
+        $this->bulb = $bulb;
+    }
+
+    public function execute()
+    {
+        $this->bulb->turnOn();
+    }
+
+    public function undo()
+    {
+        $this->bulb->turnOff();
+    }
+
+    public function redo()
+    {
+        $this->execute();
+    }
+}
+
+class TurnOff implements Command
+{
+    protected $bulb;
+
+    public function __construct(Bulb $bulb)
+    {
+        $this->bulb = $bulb;
+    }
+
+    public function execute()
+    {
+        $this->bulb->turnOff();
+    }
+
+    public function undo()
+    {
+        $this->bulb->turnOn();
+    }
+
+    public function redo()
+    {
+        $this->execute();
+    }
+}
+// Invoker
+class RemoteControl
+{
+    public function submit(Command $command)
+    {
+        $command->execute();
+    }
+}
+
+$bulb = new Bulb();
+
+$turnOn = new TurnOn($bulb);
+$turnOff = new TurnOff($bulb);
+
+$remote = new RemoteControl();
+$remote->submit($turnOn); // Bulb has been lit!
+$remote->submit($turnOff); // Darkness!
