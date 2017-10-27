6b6cda3ee4a491dd202099a1842ff15cfe706a14
Sarel van der Walt <sarel.w@gmail.com>
2017-10-27 08:44:27 +0200

    Behavioural - Template

---
+++ Behavioral/TemplateMethod/php/template.php
@@ -0,0 +1,82 @@
+<?php
+abstract class Builder
+{
+
+    // Template method
+    final public function build()
+    {
+        $this->test();
+        $this->lint();
+        $this->assemble();
+        $this->deploy();
+    }
+
+    abstract public function test();
+    abstract public function lint();
+    abstract public function assemble();
+    abstract public function deploy();
+}
+
+class AndroidBuilder extends Builder
+{
+    public function test()
+    {
+        echo 'Running android tests';
+    }
+
+    public function lint()
+    {
+        echo 'Linting the android code';
+    }
+
+    public function assemble()
+    {
+        echo 'Assembling the android build';
+    }
+
+    public function deploy()
+    {
+        echo 'Deploying android build to server';
+    }
+}
+
+class IosBuilder extends Builder
+{
+    public function test()
+    {
+        echo 'Running ios tests';
+    }
+
+    public function lint()
+    {
+        echo 'Linting the ios code';
+    }
+
+    public function assemble()
+    {
+        echo 'Assembling the ios build';
+    }
+
+    public function deploy()
+    {
+        echo 'Deploying ios build to server';
+    }
+}
+
+$androidBuilder = new AndroidBuilder();
+$androidBuilder->build();
+
+// Output:
+// Running android tests
+// Linting the android code
+// Assembling the android build
+// Deploying android build to server
+
+$iosBuilder = new IosBuilder();
+$iosBuilder->build();
+
+// Output:
+// Running ios tests
+// Linting the ios code
+// Assembling the ios build
+// Deploying ios build to server
