ae4c46340a5e41bae76b3078267722419da7f3e8
Sarel van der Walt <sarel.w@gmail.com>
2017-10-27 08:11:05 +0200

    Structural - Bridge

---
+++ Structural/Bridge/php/bridge.php
@@ -0,0 +1,71 @@
+<?php
+interface WebPage
+{
+    public function __construct(Theme $theme);
+    public function getContent();
+}
+
+class About implements WebPage
+{
+    protected $theme;
+
+    public function __construct(Theme $theme)
+    {
+        $this->theme = $theme;
+    }
+
+    public function getContent()
+    {
+        return "About page in " . $this->theme->getColor();
+    }
+}
+
+class Careers implements WebPage
+{
+    protected $theme;
+
+    public function __construct(Theme $theme)
+    {
+        $this->theme = $theme;
+    }
+
+    public function getContent()
+    {
+        return "Careers page in " . $this->theme->getColor();
+    }
+}
+
+interface Theme
+{
+    public function getColor();
+}
+
+class DarkTheme implements Theme
+{
+    public function getColor()
+    {
+        return 'Dark Black';
+    }
+}
+class LightTheme implements Theme
+{
+    public function getColor()
+    {
+        return 'Off white';
+    }
+}
+class AquaTheme implements Theme
+{
+    public function getColor()
+    {
+        return 'Light blue';
+    }
+}
+
+$darkTheme = new DarkTheme();
+
+$about = new About($darkTheme);
+$careers = new Careers($darkTheme);
+
+echo $about->getContent(); // "About page in Dark Black";
+echo $careers->getContent(); // "Careers page in Dark Black";
