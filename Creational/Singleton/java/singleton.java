8378bff0747491e6ddb2db18db902dbe942799c2
ajay <ajay_singaraju@infosys.com>
2017-10-28 15:08:15 +0530

    Bug fix for Singleton design pattern

--- Creational/Singleton/java/singleton.java
+++ Creational/Singleton/java/singleton.java
@@ -1,58 +1,40 @@
-// Java program implementing Singleton class
-// with method name as that of class
-class Singleton
-{
-	// static variable single_instance of type Singleton
-	private static Singleton single_instance=null;
+import java.io.Serializable;
 
-	// variable of type String
-	public String s;
-
-	// private constructor restricted to this class itself
-	private Singleton()
-	{
-		s = "Hello I am a string part of Singleton class";
-	}
-
-	// static method to create instance of Singleton class
-	public static Singleton Singleton()
-	{
-		// To ensure only one instance is created
-		if (single_instance == null)
-		{
-			single_instance = new Singleton();
-		}
-		return single_instance;
-	}
-}
-
+//Changing the Singleton class to Enum as it provides more flexibility of the Singleton design pattern
+// Also the class implementation of Singleton will fail during serialization.
 // Driver Code
-class Main
+public class singleton
 {
+	enum Singleton
+	{
+		INSTANCE;
+		
+		int value;
+
+		public int getValue() {
+			return value;
+		}
+		
+
+		public void setValue(int value) {
+			this.value = value;
+		}
+		
+		
+		
+	}
 	public static void main(String args[])
 	{
-		// instantiating Singleton class with variable x
-		Singleton x = Singleton.Singleton();
-
-		// instantiating Singleton class with variable y
-		Singleton y = Singleton.Singleton();
-
-		// instantiating Singleton class with variable z
-		Singleton z = Singleton.Singleton();
-
-		// changing variable of instance x
-		x.s = (x.s).toUpperCase();
-
-		System.out.println("String from x is " + x.s);
-		System.out.println("String from y is " + y.s);
-		System.out.println("String from z is " + z.s);
-		System.out.println("\n");
-
-		// changing variable of instance x
-		z.s = (z.s).toLowerCase();
-
-		System.out.println("String from x is " + x.s);
-		System.out.println("String from y is " + y.s);
-		System.out.println("String from z is " + z.s);
-	}
+		// instantiating Singleton class with variable x and set the value.
+		Singleton x = Singleton.INSTANCE;
+		x.setValue(2);
+		System.out.println("Value is  "+x.getValue());
+		
+		Singleton y = Singleton.INSTANCE;
+		System.out.println("Value is "+y.getValue());
+		
+		//As per the documentation enums are inherently serializable
+		System.out.println(x instanceof Serializable?"Yes":"No");
+		
+		}
 }
