ea8a6c53682efd7ba52043b73c609fc5c24c7c8a
satpot123 <satish.potnuru@infosys.com>
2017-10-28 15:22:17 +0530

    Added Interpreter Pattern

---
+++ Behavioral/Interpreter/InterpreterContext.java
@@ -0,0 +1,10 @@
+public class InterpreterContext {
+
+	public String getBinaryFormat(int i){
+		return Integer.toBinaryString(i);
+	}
+	
+	public String getHexadecimalFormat(int i){
+		return Integer.toHexString(i);
+	}
+}
