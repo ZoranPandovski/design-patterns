fde37885b613583fccd189d74c5477aeb49ff624
Ryan Brown <ryanbrown.dev@gmail.com>
2017-10-30 00:08:21 -0500

    perl method chaining

--- Creational/Method Chaining/perl/method_chaining.pl
+++ Creational/Method Chaining/perl/method_chaining.pl
@@ -1,3 +1,4 @@
+# Entry point for perl method chaining demo
 use strict;
 use warnings;
 
@@ -7,7 +8,9 @@ use Data::Dumper;
 $Data::Dumper::Terse = 1;
 
 my $rectangle = new Rectangle();
+# displays 0x0 rectangle
 print(Dumper($rectangle));
 
 $rectangle->setWidth(5)->setHeight(10);
+# display 5x10 rectangle
 print(Dumper($rectangle));
