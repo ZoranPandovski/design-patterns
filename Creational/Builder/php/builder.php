0d940e6e91431308dc2f315907a685e9fa68cde1
Outie Dorfling <outie.d@afrihost.com>
2017-10-30 14:30:58 +0200

    Added docblock for Creational/Builder/php/builder.php

--- Creational/Builder/php/builder.php
+++ Creational/Builder/php/builder.php
@@ -1,13 +1,36 @@
 <?php
+
+/**
+ * Class Burger
+ */
 class Burger
 {
+    /**
+     * @var int
+     */
     protected $size;
 
+    /**
+     * @var bool
+     */
     protected $cheese = false;
+    /**
+     * @var bool
+     */
     protected $pepperoni = false;
+    /**
+     * @var bool
+     */
     protected $lettuce = false;
+    /**
+     * @var bool
+     */
     protected $tomato = false;
 
+    /**
+     * Burger constructor.
+     * @param BurgerBuilder $builder
+     */
     public function __construct(BurgerBuilder $builder)
     {
         $this->size = $builder->size;
@@ -18,44 +41,81 @@ class Burger
     }
 }
 
+/**
+ * Class BurgerBuilder
+ */
 class BurgerBuilder
 {
+    /**
+     * @var int
+     */
     public $size;
 
+    /**
+     * @var bool
+     */
     public $cheese = false;
+    /**
+     * @var bool
+     */
     public $pepperoni = false;
+    /**
+     * @var bool
+     */
     public $lettuce = false;
+    /**
+     * @var bool
+     */
     public $tomato = false;
 
+    /**
+     * BurgerBuilder constructor.
+     * @param int $size
+     */
     public function __construct(int $size)
     {
         $this->size = $size;
     }
 
+    /**
+     * @return $this
+     */
     public function addPepperoni()
     {
         $this->pepperoni = true;
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     public function addLettuce()
     {
         $this->lettuce = true;
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     public function addCheese()
     {
         $this->cheese = true;
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     public function addTomato()
     {
         $this->tomato = true;
         return $this;
     }
 
+    /**
+     * @return Burger
+     */
     public function build(): Burger
     {
         return new Burger($this);
