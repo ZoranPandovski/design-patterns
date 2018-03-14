838fa56ef4f7049545ede848a5c9ad163606d329
turisap <kirill-shakirov@mail.ru>
2018-03-14 18:42:24 +0700

    command design pattern on JavaScript

---
+++ Behavioral/Command/javascript/command.js
@@ -0,0 +1,35 @@
+/**
+ * Created by HP on 3/14/2018.
+ */
+
+class Shop {
+    constructor (shopName){
+        this.shopName = shopName;
+        this.command = [];
+    }
+
+    buy(item) {
+        console.log(`Purchasing ${item} in ${this.shopName}`)
+    }
+
+    takeALook(item) {
+        console.log(`Checking out ${item} in ${this.shopName}`)
+    }
+
+    returnItem(item) {
+        console.log(`Returning ${item} in ${this.shopName}`)
+    }
+
+
+    execute(command, item) {
+        return this[command].call(this, item);
+    }
+}
+
+const sevenEleven = new Shop('7/11');
+
+sevenEleven.execute('takeALook', 'iced coffee');
+sevenEleven.execute('buy', 'sneekers');
+sevenEleven.execute('buy', 'shower gel');
+sevenEleven.execute('returnItem', 'shower gel');
+
