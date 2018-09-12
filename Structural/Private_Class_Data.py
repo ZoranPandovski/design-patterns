754db765155902d31c0c285acfaf14453b60cdf5
Nidhi Mittal <31856850+nidhimittal2@users.noreply.github.com>
2018-09-12 19:03:42 +0530

    Create Private_Class_Data.py

---
+++ Structural/Private_Class_Data.py
@@ -0,0 +1,34 @@
+lass DataClass:
+    """
+    Hide all the attributes.
+    """
+
+    def __init__(self):
+        self.value = None
+
+    def __get__(self, instance, owner):
+        return self.value
+
+    def __set__(self, instance, value):
+        if self.value is None:
+            self.value = value
+
+
+class MainClass:
+    """
+    Initialize data class through the data class's constructor.
+    """
+
+    attribute = DataClass()
+
+    def __init__(self, value):
+        self.attribute = value
+
+
+def main():
+    m = MainClass(True)
+    m.attribute = False
+
+
+if __name__ == "__main__":
+    main()
