8c3567126f7ec355dd3f905e4b12545dd73c3a06
Nidhi Mittal <31856850+nidhimittal2@users.noreply.github.com>
2018-09-12 19:50:36 +0530

    Update Private_Class_Data.py
    The private class data design pattern solves the problems above by extracting a data class for the target class and giving the target class instance an instance of the extracted data class.

--- Structural/Private_Class_Data.py
+++ Structural/Private_Class_Data.py
@@ -1,3 +1,11 @@
+/* A class may expose its attributes (class variables) to manipulation when manipulation is no longer desirable,
+e.g. after construction. Using the private class data design pattern prevents that undesirable manipulation.
+A class may have one-time mutable attributes that cannot be declared final.
+Using this design pattern allows one-time setting of those class attributes.
+The motivation for this design pattern comes from the design goal of protecting class state by minimizing
+the visibility of its attributes (data). */
+//CODE:
+    
 lass DataClass:
     """
     Hide all the attributes.
