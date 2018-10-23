e31416f8cba04bafc75909c8c45a5b6aed08b4b3
daprieto1 <da.prieto1@uniandes.edu.co>
2018-10-23 11:13:35 +0200

    fix some java mistakes and create a more clear example for the template method pattern

--- Behavioral/TemplateMethod/java/Midfielder.java
+++ Behavioral/TemplateMethod/java/Midfielder.java
@@ -1,19 +1,14 @@
 /**
  * A midfielder is a player but has unique properties
+ * 
  * @author sherilpaulin
  *
  */
-public class Midfielder {
-	private String name;
-	private int number;
-	public Midfielder(String name, int number){
-		this.name = name;
-		this.number = number;
-	}
+public class Midfielder extends Player {
+
 	@Override
-	public String getMove(){
-		return "Create plays";
+	public void heavyTraining() {
+		System.out.println("This is a MIDFIELDER making heavy defence training");
 	}
-	
 
 }
--- Behavioral/TemplateMethod/java/TemplateMethodDemo.java
+++ Behavioral/TemplateMethod/java/TemplateMethodDemo.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * same usage of this pattern
  * @author sherilpaulin
@@ -5,13 +7,14 @@
  */
 public class TemplateMethodDemo {
 	public static void main(String[] args) {
-		List<Player> players = new ArrayList<Player>;
+		
+		List<Player> players = new ArrayList<Player>();
+
 		players.add(new Defender("Sergio Ramos", 4));
 		players.add(new Midfielder("Luka Modric",10));
 		players.add(new Goalkeeper("Keylor Navas", 1));
-		for (Player player: players){
-			System.out.println(String.format("%s #%d", player.getName(), player.getNumber()));
-			System.out.println(player.getMove());
-		}
+
+		players.stream().forEach(p -> p.permormTrainingSession());
+
 	}
 }
