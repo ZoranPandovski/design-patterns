c93554c514417fdbeb180baeb7ad4a0640d32efb
Luca Suriano <lucasuriano@live.it>
2018-10-24 22:51:08 +0200

    Factory pattern in go

---
+++ Creational/Factory/go/book_factory.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+    "reflect"
+)
+
+type Book struct {
+  Title string
+  Author string
+  Pages int
+}
+
+func BookFactory(title string, author string, pages int) Book {
+  return Book{title, author, pages}
+}
+
+func main() {
+	new_book := BookFactory("Harry Potter and the Philosopher's Stone", "J.K. Rawling", 223)
+	fmt.Println(new_book)
+	fmt.Println(reflect.TypeOf(new_book))
+}
