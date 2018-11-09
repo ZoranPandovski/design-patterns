6d8f55b009a2bfa52857bf4b84a555bf3c3e70d9
Naveen K <naveen.hopetech@gmail.com>
2018-11-09 14:51:23 -0800

    Created a calculator using the factory design pattern. This will peform sums, differences, products, quotients, and exponents

---
+++ Creational/Abstract Factory/java/CalculatorFactory/Add.java
@@ -0,0 +1,11 @@
+//package com.nk.springboot.designpatterns.Factory;
+
+public class Add extends Calculator {
+
+	@Override
+	public int calculate() {
+		// TODO Auto-generated method stub
+		return a + b;
+	}
+
+}
