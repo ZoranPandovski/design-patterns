f17abd84b0996174e1bc022aa6a276bebea8aa1a
jonasvag <jonasvag777@gmail.com>
2019-10-01 21:06:08 +0300

    Add template method in C# language

---
+++ Behavioral/TemplateMethod/c#/BaseOrder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    public abstract class BaseOrder
+    {
+        private readonly decimal _basePrice;
+
+        public BaseOrder(decimal basePrice)
+        {
+            _basePrice = basePrice;
+        }
+
+        public bool PlaceOrder()
+        {
+            PrintBeforeOrder();
+
+            if (!ConfirmOrder(_basePrice))
+            {
+                return false;
+            }
+
+            PrintAfterOrder();
+
+            return true;
+        }
+
+        private void PrintBeforeOrder()
+        {
+            Console.WriteLine("Initializing the order...");
+        }
+
+        private void PrintAfterOrder()
+        {
+            Console.WriteLine("Finishing the order...");
+        }
+
+        protected abstract bool ConfirmOrder(decimal price);
+    }
+}
+
+
---
+++ Behavioral/TemplateMethod/c#/DerivedOrder2.cs
@@ -0,0 +1,19 @@
+﻿namespace TemplateMethod
+{
+    public class DerivedOrder2 : BaseOrder
+    {
+        public DerivedOrder2(decimal basePrice) : base(basePrice)
+        {
+
+        }
+
+        protected override bool ConfirmOrder(decimal price)
+        {
+            if (price >= 0 && price < 999)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
