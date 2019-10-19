e55c0bf895de59e2cf5e3594c30c9595d0f062a9
mouse <bob.mouse@gmail.com>
2019-10-20 00:47:06 +0100

    Abstract Factory design pattern in C#

---
+++ Creational/Abstract Factory/csharp/csharp/Cars/Opel.cs
@@ -0,0 +1,58 @@
+﻿using csharp.Cars.Properties;
+using csharp.Factory;
+
+namespace csharp.Cars
+{
+    public abstract class Opel : Car
+    {
+        private int TopSpeed;
+        private int CurrentSpeed;
+        private int SpeedVariation;
+        private Color Color { get; }
+        private string Brand { get; }
+
+        public Opel(Color color, int topSpeed, int speedVariation)
+        {
+            this.TopSpeed = topSpeed;
+            this.CurrentSpeed = 0;
+            SpeedVariation = speedVariation;
+            Brand = this.GetType().BaseType.Name;
+            Color = color;
+        }
+
+        public string Accelerate()
+        {
+            if (CurrentSpeed >= TopSpeed)
+                return $"Going full speed at {CurrentSpeed}";
+
+            CurrentSpeed += SpeedVariation;
+            return $"Accelerating. Going at {CurrentSpeed}";
+        }
+
+        public string Break()
+        {
+            if (CurrentSpeed <= 0)
+                return "Stopped";
+
+            CurrentSpeed -= SpeedVariation;
+            return $"Decreasing speed to {CurrentSpeed}";
+        }
+
+        public string GetBrand()
+        {
+            return Brand;
+        }
+
+        public string GetColor()
+        {
+            return Color.ToString();
+        }
+
+        public abstract string GetModel();
+
+        public int GetCurrentSpeed()
+        {
+            return CurrentSpeed;
+        }
+    }
+}
---
+++ Creational/Abstract Factory/csharp/csharp/Cars/Properties/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp.Cars.Properties
+{
+    public enum Model
+    {
+        CORSA,
+        ASTRA,
+        VECTRA,
+        ZAFIRA
+    }
+}
