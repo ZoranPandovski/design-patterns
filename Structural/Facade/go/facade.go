e2ad057d8d9b6956ae94ebb145c4f24772cf0950
Patrick <patrick@lincros.com>
2019-10-19 15:21:57 -0300

    Added an example of the Façade pattern in Go

--- Structural/Facade/go/facade.go
+++ Structural/Facade/go/facade.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//All of theses are the subsystems of the façade
 type cpu struct {
 }
 
@@ -37,6 +38,8 @@ func (h *hardDrive) read(startPosition int, size int) string {
 func (h *hardDrive) write(startPosition int, size int) {
 	fmt.Println("Writing data into the HD")
 }
+
+//End of the subsystems of the façade
 
 //ComputerFacade is the façade that hides all the complexity of the subsystems that makes the computer work
 type ComputerFacade struct {
