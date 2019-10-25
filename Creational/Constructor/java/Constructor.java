68341320cac0daf8a2f6daeff2421999cba55f4c
Martina Catizone <martina.catizone@student.manchester.ac.uk>
2019-10-25 22:22:14 +0100

    add constructor pattern in java

---
+++ Creational/Constructor/java/Constructor.java
@@ -0,0 +1,12 @@
+public class Constructor {
+	private int name;
+
+	public Constructor() {
+		// construct the 'Constructor' object
+	}
+
+	public Constructor(String name) {
+		// constructors can be overloaded
+		this.name = name;
+	}
+}
