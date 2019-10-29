65d106afbe4648f92401378bc97ff51becef53d4
snunsan <snunsan@gmail.com>
2019-10-29 18:42:06 +0100

    Refactor to fix compilation

--- Creational/Dependency Injection/java/DependencyInjectorPattern.java
+++ Creational/Dependency Injection/java/DependencyInjectorPattern.java
@@ -1,100 +1,107 @@
-/**
- * Contract for service implementations
- */
-public interface MessageService {
-    void sendMessage(String msg, String receiver);
-}
-
-/**
- * Consumer interface declaring contract for consumer classes
- */
-public interface Consumer {
-    void processMessages(String msg, String rec);
-}
-
-/**
- * An interface MessageServiceInjector with method declaration that returns the {@link Consumer} class.
- */
-public interface MessageServiceInjector {
-    public Consumer getConsumer();
-}
 
 
 /**
- * Email services that implement {@link MessageService}
+ * Dependency Injector pattern demonstration.
  */
-public class EmailServiceImpl implements MessageService {
+public class DependencyInjectorPattern {
 
-    @Override
-    public void sendMessage(String msg, String receiver) {
-        System.out.println("Email sent to " + receiver + " with Message=" + msg);
-    }
+	/**
+	 * Contract for service implementations
+	 */
+	public interface MessageService {
+	    void sendMessage(String msg, String receiver);
+	}
 
-}
+	/**
+	 * Consumer interface declaring contract for consumer classes
+	 */
+	public interface Consumer {
+	    void processMessages(String msg, String rec);
+	}
 
-/**
- * Injector class for {@link EmailServiceImpl}
- */
-public class EmailServiceInjector implements MessageServiceInjector {
-
-    @Override
-    public Consumer getConsumer() {
-        return new MyConsumer(new EmailServiceImpl());
-    }
-}
-
-
-/**
- * SMS services that implement {@link MessageService}
- */
-public class SMSServiceImpl implements MessageService {
-
-    @Override
-    public void sendMessage(String msg, String receiver) {
-        System.out.println("SMS sent to " + receiver + " with Message=" + msg);
-    }
-
-}
-
-/**
- * Injector class for {@link SMSServiceImpl}
- */
-public class SMSServiceInjector implements MessageServiceInjector {
-
-    @Override
-    public Consumer getConsumer() {
-        return new MyConsumer(new SMSServiceImpl());
-    }
-
-}
+	/**
+	 * An interface MessageServiceInjector with method declaration that returns the {@link Consumer} class.
+	 */
+	public interface MessageServiceInjector {
+	    public Consumer getConsumer();
+	}
 
 
-/**
- * Implementation of {@link Consumer}. Notice that this class is just using the service. It does not initialize the service that leads to better â€œseparation of concernsâ€œ.
- */
-public class MyConsumer implements Consumer {
+	/**
+	 * Email services that implement {@link MessageService}
+	 */
+	public static class EmailServiceImpl implements MessageService {
 
-    private MessageService service;
+	    @Override
+	    public void sendMessage(String msg, String receiver) {
+	        System.out.println("Email sent to " + receiver + " with Message=" + msg);
+	    }
 
-    public MyConsumer(MessageService messageService) {
-        this.service = messageService;
-    }
+	}
 
-    @Override
-    public void processMessages(String msg, String receiver) {
-        this.service.sendMessage(msg, receiver);
-    }
-}
+	/**
+	 * Injector class for {@link EmailServiceImpl}
+	 */
+	public static class EmailServiceInjector implements MessageServiceInjector {
 
-/**
- * Client application
- */
-public class MyApplication {
+	    @Override
+	    public Consumer getConsumer() {
+	        return new MyConsumer(new EmailServiceImpl());
+	    }
+	}
 
+
+	/**
+	 * SMS services that implement {@link MessageService}
+	 */
+	public static class SMSServiceImpl implements MessageService {
+
+	    @Override
+	    public void sendMessage(String msg, String receiver) {
+	        System.out.println("SMS sent to " + receiver + " with Message=" + msg);
+	    }
+
+	}
+
+	/**
+	 * Injector class for {@link SMSServiceImpl}
+	 */
+	public static class SMSServiceInjector implements MessageServiceInjector {
+
+	    @Override
+	    public Consumer getConsumer() {
+	    	
+	        return new MyConsumer(new SMSServiceImpl());
+	    }
+
+	}
+
+
+	/**
+	 * Implementation of {@link Consumer}. Notice that this class is just using the service.
+	 * It does not initialize the service that leads to better “separation of concerns“.
+	 */
+	public static class MyConsumer implements Consumer {
+
+		// The service is declared by means of an interface but it injected with a concrete instance
+	    private MessageService service;
+
+	    public MyConsumer(MessageService messageService) {
+	        this.service = messageService;
+	    }
+
+	    @Override
+	    public void processMessages(String msg, String receiver) {
+	        this.service.sendMessage(msg, receiver);
+	    }
+	}
+	
     public static void main(String[] args) {
         String msg = "This is injected ";
         String email = "riteshakya037@gmail.com";
         String phone = "+9779841814809";
+
+        // The service is declared by means of an interface but it created with a concrete instance
         MessageServiceInjector injector = null;
         Consumer app = null;
 
