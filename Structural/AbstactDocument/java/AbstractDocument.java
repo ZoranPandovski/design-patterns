64d0dd7e282706d97b0fa36673cbff943093adb3
Sardor <54173813+Sardor-Karimboyev@users.noreply.github.com>
2021-10-07 17:41:06 +0500

    added AbstractDocument *.java

---
+++ Structural/AbstactDocument/java/AbstractDocument.java
@@ -0,0 +1,49 @@
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public abstract class AbstractDocument implements Document {
+
+  private final Map<String, Object> properties;
+
+  protected AbstractDocument(Map<String, Object> properties) {
+    Objects.requireNonNull(properties, "properties map is required");
+    this.properties = properties;
+  }
+
+  @Override
+  public Void put(String key, Object value) {
+    properties.put(key, value);
+    return null;
+  }
+
+  @Override
+  public Object get(String key) {
+    return properties.get(key);
+  }
+
+  @Override
+  public <T> Stream<T> children(String key, Function<Map<String, Object>, T> constructor) {
+    return Stream.ofNullable(get(key))
+        .filter(Objects::nonNull)
+        .map(el -> (List<Map<String, Object>>) el)
+        .findAny()
+        .stream()
+        .flatMap(Collection::stream)
+        .map(constructor);
+  }
+
+  @Override
+  public String toString() {
+    var builder = new StringBuilder();
+    builder.append(getClass().getName()).append("[");
+    properties.forEach((key, value) -> builder.append("[").append(key).append(" : ").append(value)
+        .append("]"));
+    builder.append("]");
+    return builder.toString();
+  }
+
+}
