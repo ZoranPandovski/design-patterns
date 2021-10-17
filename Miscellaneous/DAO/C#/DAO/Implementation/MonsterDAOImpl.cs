f6bd75d15f5153c54be33bdf33ffa569119a898f
Scott Rallya <srallya@gmail.com>
2021-10-16 20:58:42 -0400

    DAO Implementation for C#
    A Sample DAO Implementation for C#

---
+++ Miscellaneous/DAO/C#/DAO/Implementation/MonsterDAOImpl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAO.Interfaces;
+
+namespace DAO.Implementation
+{
+    class MonsterDAOImpl : MonsterDAO
+    {
+        private List<Monster> Monsters = new List<Monster>();
+
+        public void DeleteMonsterByName(string name)
+        {
+            Monster monster = Monsters.Find(x => x.Name == name);
+            Monsters.Remove(monster);
+        }
+
+        public List<Monster> GetAllMonsters()
+        {
+            return Monsters;
+        }
+
+        public Monster GetMonsterByName(string name)
+        {
+            Monster monster = Monsters.Find(x => x.Name == name);
+            return monster;
+        }
+
+        public List<Monster> GetMonstersByLevel(int level)
+        {
+            List<Monster> monsters = Monsters.FindAll(x => x.Level == level);
+            return monsters;
+        }
+
+        public void SaveMonster(Monster monster)
+        {
+            Monsters.Add(monster);
+        }
+    }
+}
---
+++ Miscellaneous/DAO/C#/DAO/Monster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAO
+{
+    class Monster
+    {
+        public string Name { get; private set; }
+        public int Level { get; private set; }
+        public int HitPoints { get; private set; }
+        public string Description { get; private set; }
+
+        public Monster() { }
+
+        public Monster(string name, int level, int hitPoints, string description)
+        {
+            Name = name;
+            Level = level;
+            HitPoints = hitPoints;
+            Description = description;
+        }
+    }
+}
