18977909e51bc4b113ec58e9a719b5e60d65ce6f
Ruchika Salwan <ruchikasalwan80@gmail.com>
2021-10-27 12:29:06 +0530

    correcting class creation

--- Creational/Singleton/java/SingletonDesignPattern.java
+++ Creational/Singleton/java/SingletonDesignPattern.java
@@ -1,24 +1,24 @@
 public class SingletonDesignPattern {
+    private static class Singleton {
+        private static Singleton INSTANCE;
 
+        private Singleton() {
+        }
+
+        public static Singleton getSingletonInstance() {
+            if (INSTANCE == null) {
+                INSTANCE = new Singleton();
+            }
+            return INSTANCE;
+        }
+
+        public void printSingleton() {
+            System.out.println("Singleton Class");
+        }
+    }
     public static void main(String[] args) {
         Singleton singleton = Singleton.getSingletonInstance();
         singleton.printSingleton();
     }
-}
 
-class Singleton {
-    private static Singleton INSTANCE;
-
-    private Singleton() {}
-
-    public static Singleton getSingletonInstance() {
-        if(INSTANCE == null) {
-            INSTANCE = new Singleton();
-        }
-        return INSTANCE;
-    }
-
-    public void printSingleton() {
-        System.out.println("Singleton Class");
-    }
 }
