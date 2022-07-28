ba78913cfa1f64df27d82cee3a1fed57fcbce87c
kalpeshjethwa <kalpesh.jethwa@priceline.com>
2022-07-28 16:54:12 +0530

    Create Singleton service using enum

---
+++ Creational/Singleton/java/EnumSingleton.java
@@ -0,0 +1,10 @@
+/**
+ * Create singleton service using enum
+ */
+public enum EnumSingleton {
+    INSTANCE;
+
+    public static void displayResult(String result) {
+        System.out.println(result);
+    }
+}
